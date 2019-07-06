15fcfd8f6b53b0d67137e7f18de7a7060faca052
Joshua Kaluski <joshua@kaluski.com>
2019-07-06 18:45:40 -0400

    Prevent duplicate users & implement serializeUser

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,10 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const { googleClientID, googleClientSecret } = require("../config/keys");
 const User = require('../models/User');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 //Set up passport to use Google OAuth
 passport.use(
   new GoogleStrategy(
@@ -11,8 +15,19 @@ passport.use(
       clientSecret: googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id}).save();
+     async (accessToken, refreshToken, profile, done) => {
+      let existingUser = await User.findOne({googleID: profile.id});
+
+      if (!existingUser) {
+        //Exit callback with existing User
+        done(null, existingUser);
+      } else {
+        //Create a new User if none exist
+        let user = await new User({googleID: profile.id}).save();
+
+        //Exit callback with new User
+        done(null, user);
+      }
     }
   )
 );
