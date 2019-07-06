f5299d102e9c5f5d5babe74567c7eeea75d3c5ee
Joshua Kaluski <joshua@kaluski.com>
2019-07-06 17:00:40 -0400

    Set up Mongoose

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const mongoose = require('mongoose');
 require('./services/passport');
+const {mongoURI} = require('./config/keys');
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(mongoURI);
 
 const app = express();
 
