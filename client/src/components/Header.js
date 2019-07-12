ad3dd235aa525cd938ea000ac7eec78bae1e315b
Joshua Kaluski <joshua@kaluski.com>
2019-07-12 19:17:46 -0400

    Implemented Link on Emaily

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -22,10 +23,17 @@ class Header extends Component {
   }
 
   render() {
+
+
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul id="nav-mobile" className="right">
             {this.renderContent()}
           </ul>
